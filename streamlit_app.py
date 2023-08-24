12d7cc2b25feb6168167e6bdbe2dd19f4ca46455
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-08-24 15:40:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import streamlit as st
 
-tokenizer = AutoTokenizer.from_pretrained("TheBloke/CodeLlama-13B-Python-fp16")
-model = AutoModelForCausalLM.from_pretrained("TheBloke/CodeLlama-13B-Python-fp16")
+@st.cache_data
+def load_model():
+  tokenizer = AutoTokenizer.from_pretrained("TheBloke/CodeLlama-13B-Python-fp16")
+  model = AutoModelForCausalLM.from_pretrained("TheBloke/CodeLlama-13B-Python-fp16")
+  return tokenizer, model
+  
+tokenizer, model = load_model()
+
+prompt = st.text_input("your prompt)
