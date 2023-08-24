61f982415353663128c9af0900e521b8404a2e71
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-08-24 15:52:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 @st.cache_data
 def load_model():
   tokenizer = AutoTokenizer.from_pretrained("TheBloke/CodeLlama-13B-Python-fp16")
-  model = AutoModelForCausalLM.from_pretrained("TheBloke/CodeLlama-13B-Python-fp16")
+  model = AutoModelForCausalLM.from_pretrained("TheBloke/CodeLlama-13B-Python-fp16", ignore_mismatched_sizes=True)
   return tokenizer, model
   
 tokenizer, model = load_model()
