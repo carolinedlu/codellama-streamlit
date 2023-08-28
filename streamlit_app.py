bdba0e7d88b2d614b333acab65fa68e5f14937ee
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-08-28 10:29:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ def generate_codellama_response(prompt_input):
     #                        input={"prompt": f"{prompt_input}",
     #                               "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
       output = replicate.run('replicate/codellama-13b:1c914d844307b0588599b8393480a3ba917b660c7e9dfae681542b5325f228db', 
-                           input={"prompt": f"{prompt_input}",
-                           "temperature":0.75, "max_length":512, "repetition_penalty":1})
+                           input={"prompt": f"{prompt_input}"})
+                           # "temperature":0.75, "max_length":512, "repetition_penalty":1})
     
       return output
 
